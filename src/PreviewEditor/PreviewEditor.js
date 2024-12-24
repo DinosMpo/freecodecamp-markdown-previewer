4db4f9205fe77ac6a4728a8b401e359787685a76
DinosMpo <dinos7591@yahoo.gr>
2024-12-24 12:03:43 +0200

    fix an issue

--- src/PreviewEditor/PreviewEditor.js
+++ src/PreviewEditor/PreviewEditor.js
@@ -1,12 +1,11 @@
 import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
 import './PreviewEditor.css';
 
 export default function PreviewEditor({text}) {
   return (
     <div id="previewEditorContainer">
       <div id="previewEditorTitle">Preview</div>
-      <div id="preview"><Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown></div>
+      <div id="preview"><Markdown>{text}</Markdown></div>
     </div>
   )
 }
